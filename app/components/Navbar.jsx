ef631ab59b0fa2bd7ca2e695ee8e126cae11118f
Beatrice Wambui Mbugua <wambuimbeatrice@gmail.com>
2023-08-23 09:59:59 +0300

    adjust the icon sizes and align items center

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function Navbar() {
   return (
     <nav className="w-full py-9">
-      <div className="text-white container mx-auto flex justify-around">
+      <div className="text-white container mx-auto flex justify-between items-center">
         <div>
           <Link href={"/"}>
             <Image
@@ -13,10 +13,11 @@ export default function Navbar() {
               width={151}
               height={102}
               alt="logo"
+              className="w-28"
             />
           </Link>
         </div>
-        <div className="flex gap-8">
+        <div className="flex gap-8 text-lg">
           <Link href={"#"}>
             <h1>Home</h1>
           </Link>
@@ -27,7 +28,7 @@ export default function Navbar() {
             <h1>Products</h1>
           </Link>
           <Link href={"#"}>
-            <h1>Contact US</h1>
+            <h1>Contact</h1>
           </Link>
         </div>
         <div className="flex gap-8">
@@ -36,30 +37,35 @@ export default function Navbar() {
             width={20}
             height={20}
             alt="search"
+            className="w-5 h-5"
           />
           <Image
             src={"/assets/icons/user.svg"}
             width={20}
             height={20}
             alt="user"
+            className="w-5 h-5"
           />
           <Image
             src={"/assets/icons/heart.svg"}
             width={37}
             height={35}
             alt="heart"
+            className="w-6 h-6"
           />
           <Image
             src={"/assets/icons/cart.svg"}
             width={37}
             height={37}
             alt="cart"
+            className="w-6 h-6"
           />
           <Image
             src={"/assets/icons/lightmode.svg"}
             width={24}
             height={24}
             alt="light mode"
+            className="w-5 h-5"
           />
         </div>
       </div>
